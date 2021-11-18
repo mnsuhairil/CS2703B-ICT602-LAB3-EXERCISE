2c9795a8f6a6b6b6fdcade6966c4f9720c5acc60
mnsuhairil <2020897584@student.uitm.edu.my>
2021-11-19 03:26:16 +0800

    LAB3SUHAIRIL

---
+++ app/src/main/java/com/example/lab3_offline_database_suhairil/SecondFragment.java
@@ -0,0 +1,16 @@
+package com.example.lab3_offline_database_suhairil;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.lab3_offline_database_suhairil.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+}
